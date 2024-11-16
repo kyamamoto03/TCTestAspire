61a2875e9763624c2ca7d6b1a5053f3c09e50cdc
山本 浩司 <k.yamamoto03@iforcom.jp>
2024-11-16 17:19:16 +0900

    PostUserTest追加

--- TCTest.WebApi.Test/GetUserTest.cs
+++ TCTest.WebApi.Test/GetUserTest.cs
@@ -3,30 +3,29 @@ using TCTest.Infra;
 using TCTest.Infra.Repository;
 using TCTest.WebApi.Apis;
 
-namespace TCTest.WebApi.Test
+namespace TCTest.WebApi.Test;
+
+public class GetUserTest : DbInstance
 {
-    public class GetUserTest : DbInstance
+    public TCTestDB CreateTcTestDB()
     {
-        public TCTestDB CreateTcTestDB()
-        {
-            var _db = new TCTestDB(new DbContextOptionsBuilder<TCTestDB>()
-               .UseNpgsql(DbConnectionString)
-               .Options);
+        var _db = new TCTestDB(new DbContextOptionsBuilder<TCTestDB>()
+           .UseNpgsql(DbConnectionString)
+           .Options);
 
-            return _db;
-        }
+        return _db;
+    }
 
-        [Fact]
-        public async Task 全件取得_1件以上データがあること()
-        {
-            // Arrange
-            var db = CreateTcTestDB();
+    [Fact]
+    public async Task 全件取得_1件以上データがあること()
+    {
+        // Arrange
+        var db = CreateTcTestDB();
 
-            // Act
-            var users = await UserApi.GetUserAsync(new UserRepository(db));
+        // Act
+        var users = await UserApi.GetUserAsync(new UserRepository(db));
 
-            // Assert
-            Assert.NotNull(users);
-        }
+        // Assert
+        Assert.NotNull(users);
     }
 }
---
+++ TCTest.WebApi.Test/PostUserTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TCTest.Domain.UserModel;
+using TCTest.DTO;
+using TCTest.Infra;
+using TCTest.Infra.Repository;
+using TCTest.WebApi.Apis;
+
+namespace TCTest.WebApi.Test;
+
+public class PostUserTest : DbInstance
+{
+    public TCTestDB CreateTcTestDB()
+    {
+        var _db = new TCTestDB(new DbContextOptionsBuilder<TCTestDB>()
+           .UseNpgsql(DbConnectionString)
+           .Options);
+
+        return _db;
+    }
+
+    [Fact]
+    public async Task データを投入()
+    {
+        // Arrange
+        var db = CreateTcTestDB();
+        var UserId = "PostUser01";
+        var user = new PostUserRequest
+        {
+            UserId = UserId,
+            Name = "PostUserName",
+            Age = 50
+        };
+
+        IUserRepository userRepository = new UserRepository(db);
+        // Act
+        await UserApi.PostUserAsync(user, userRepository);
+
+        var addUser = await UserApi.GetUserAtAsync(UserId, userRepository);
+
+        // Assert
+        Assert.NotNull(addUser);
+        Assert.Equal(UserId, addUser.UserId);
+        Assert.Equal(user.Name, addUser.Name);
+        Assert.Equal(user.Age, addUser.Age);
+    }
+}
